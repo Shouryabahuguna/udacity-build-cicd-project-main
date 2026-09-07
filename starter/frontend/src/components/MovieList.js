98da211fc06ed527c07a656a5fdb201380c2ee0a
Shouryabahuguna <shouryabahuguna0@gmail.com>
2026-09-07 16:21:53 +0000

    fix(frontend): format MovieList with single quote for prettier

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -6,18 +6,26 @@ function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    const backendUrl = "http://a42a20cafe6054347ac993453fdba15d-903599057.us-east-1.elb.amazonaws.com";
-    axios.get(`${backendUrl}/movies`).then((response) => {
-      setMovies(response.data.movies);
-    }).catch((err) => {
-      console.error("Failed to fetch movies:", err);
-    });
+    axios
+      .get(
+        'http://a42a20cafe6054347ac993453fdba15d-903599057.us-east-1.elb.amazonaws.com/movies'
+      )
+      .then((response) => {
+        setMovies(response.data.movies);
+      })
+      .catch((err) => {
+        console.error('Failed to fetch movies:', err);
+      });
   }, []);
 
   return (
     <ul>
       {movies.map((movie) => (
-        <li className="movieItem" key={movie.id} onClick={() => onMovieClick(movie)}>
+        <li
+          className="movieItem"
+          key={movie.id}
+          onClick={() => onMovieClick(movie)}
+        >
           {movie.title}
         </li>
       ))}
