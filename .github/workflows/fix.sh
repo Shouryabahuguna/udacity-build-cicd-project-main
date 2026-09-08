3c8fc5a9163818af7254e4557a44c1c3ca64ca42
Shouryabahuguna <shouryabahuguna@gmail.com>
2026-09-08 04:38:10 +0000

    fix: update workflows per rubric requirement

--- .github/workflows/fix.sh
+++ .github/workflows/fix.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
 set -e
 
-git config user.name "Shouryabahuguna"
-git config user.email "shouryabahuguna@gmail.com"
+echo "1. Applying aws-auth ConfigMap to link worker nodes..."
+cat << 'EOF' | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: arn:aws:iam::803525118502:role/udacity-node-group
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+  mapUsers: |
+    - userarn: arn:aws:iam::803525118502:user/github-action-user
+      username: github-action-role
+      groups:
+        - system:masters
+EOF
 
-git add -A
-git commit -m "fix: frontend-ci build job test step added"
-git push origin main
+echo ""
+echo "2. Waiting 10 seconds for node registration..."
+sleep 10
+
+echo ""
+echo "=========================================="
+echo ">> KUBECTL GET NODES <<"
+echo "=========================================="
+kubectl get nodes
+
+echo ""
+echo "=========================================="
+echo ">> KUBECTL GET PODS -O WIDE <<"
+echo "=========================================="
+kubectl get pods -o wide
